e72093478cdb6399e05f8f58760d875953a06ee4
George Sykes <georgesykes86@gmail.com>
2017-11-12 19:15:19 +0000

    Grid update

---
+++ mysitejavascript.js
@@ -0,0 +1,9 @@
+function on() {
+    document.getElementById("hidden").style.display = "block";
+    document.getElementById("expand").innerHTML = "Read less";
+}
+
+function off() {
+    document.getElementById("hidden").style.display = "none";
+    document.getElementById("expand").innerHTML = "Read more";
+}
