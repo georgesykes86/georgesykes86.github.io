3a0682feeec7f9066b8ea54abed0d0d00c730db9
George Sykes <georgesykes86@gmail.com>
2018-03-11 19:01:46 +0000

    update js

--- mysitejavascript.js
+++ mysitejavascript.js
@@ -27,78 +27,69 @@ function advButtonClick() {
 
 }
 
-$('#family').toggle(function() {
-  $('#well-right').css('visibility', 'visible');
-  $('#family_det').css('display', 'block');
-}, function() {
-  // on mouseout, reset the background colour
-  $('#well-right').css('visibility', 'hidden');
-  $('#family_det').css('display', 'none');
-});
 
-$('#address').toggle(function() {
-  $('#well-right').css('visibility', 'visible');
-  $('#address_det').css('display', 'block');
-}, function() {
-  // on mouseout, reset the background colour
-  $('#well-right').css('visibility', 'hidden');
-  $('#address_det').css('display', 'none');
-});
+function handler1() {
+	switch(this.id) {
+    case "#family":
+			$('#well-right').css('visibility', 'visible');
+			$('#family_det').css('display', 'block');
+        break;
+    case "#address":
+      $('#well-right').css('visibility', 'visible');
+      $('#address_det').css('display', 'block');
+        break;
+    case "#education":
+     	$('#well-right').css('visibility', 'visible');
+      $('#edu_det').css('display', 'block');
+        break;
+    case "#work":
+     	$('#well-left').css('visibility', 'visible');
+     	$('#jobs_det').css('display', 'block');
+        break;
+    case "#sport":
+    	$('#well-left').css('visibility', 'visible');
+    	$('#sport_det').css('display', 'block');
+        break;
+    case "#films":
+      $('#well-left').css('visibility', 'visible');
+     	$('#film_det').css('display', 'block');
+        break;
+    default:
+	};
+	$(".small_img").one("click", handler2);
+}
 
-$('#education').toggle(function() {
-  $('#well-right').css('visibility', 'visible');
-  $('#edu_det').css('display', 'block');
-}, function() {
-  // on mouseout, reset the background colour
-  $('#well-right').css('visibility', 'hidden');
-  $('#edu_det').css('display', 'none');
-});
+function handler2() {
+	switch(this.id) {
+  case "#family":
+		$('#well-right').css('visibility', 'hidden');
+		$('#family_det').css('display', 'none');
+      break;
+  case "#address":
+    $('#well-right').css('visibility', 'hidden');
+    $('#address_det').css('display', 'none');
+      break;
+  case "#education":
+   	$('#well-right').css('visibility', 'hidden');
+    $('#edu_det').css('display', 'none');
+      break;
+  case "#work":
+   	$('#well-left').css('visibility', 'hidden');
+   	$('#jobs_det').css('display', 'none');
+      break;
+  case "#sport":
+  	$('#well-left').css('visibility', 'hidden');
+  	$('#sport_det').css('display', 'none');
+      break;
+  case "#films":
+    $('#well-left').css('visibility', 'hidden');
+   	$('#film_det').css('display', 'none');
+      break;
+  default:
+	};
+	$(".small_img").one("click", handler1);
+}
 
-$('#work').toggle(function() {
-  $('#well-left').css('visibility', 'visible');
-  $('#jobs_det').css('display', 'block');
-}, function() {
-  // on mouseout, reset the background colour
-  $('#well-left').css('visibility', 'hidden');
-  $('#jobs_det').css('display', 'none');
-});
 
-$('#sport').toggle(function() {
-  $('#well-left').css('visibility', 'visible');
-  $('#sport_det').css('display', 'block');
-}, function() {
-  // on mouseout, reset the background colour
-  $('#well-left').css('visibility', 'hidden');
-  $('#sport_det').css('display', 'none');
-});
-
-$('#films').toggle(function() {
-  $('#well-left').css('visibility', 'visible');
-  $('#film_det').css('display', 'block');
-}, function() {
-  // on mouseout, reset the background colour
-  $('#well-left').css('visibility', 'hidden');
-  $('#film_det').css('display', 'none');
-});
-
-$(function() {
-	$('#family').click(function() {
-		$(this).toggle();
-	});
-	$('#address').click(function() {
-		$(this).toggle();
-	});
-	$('#education').click(function() {
-		$(this).toggle();
-	});
-	$('#work').click(function() {
-		$(this).toggle();
-	});
-	$('#sport').click(function() {
-		$(this).toggle();
-	});
-	$('#films').click(function() {
-		$(this).toggle();
-	});
-});
+$(".small_img").one("click", handler1);
 
