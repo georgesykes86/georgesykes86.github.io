7ce3cdefd1891359b79c77e9cf7babf25e1e0716
George Sykes <georgesykes86@gmail.com>
2018-03-11 18:27:42 +0000

    update js

--- mysitejavascript.js
+++ mysitejavascript.js
@@ -27,70 +27,78 @@ function advButtonClick() {
 
 }
 
-$(function() {
-  $('#family').click(function() {
-    $('#well-right').css('visibility', 'visible');
-    $('#family_det').css('display', 'block');
-  }, function() {
-    // on mouseout, reset the background colour
-    $('#well-right').css('visibility', 'hidden');
-    $('#family_det').css('display', 'none');
-  });
+$('#family').toggle(function() {
+  $('#well-right').css('visibility', 'visible');
+  $('#family_det').css('display', 'block');
+}, function() {
+  // on mouseout, reset the background colour
+  $('#well-right').css('visibility', 'hidden');
+  $('#family_det').css('display', 'none');
+});
+
+$('#address').toggle(function() {
+  $('#well-right').css('visibility', 'visible');
+  $('#address_det').css('display', 'block');
+}, function() {
+  // on mouseout, reset the background colour
+  $('#well-right').css('visibility', 'hidden');
+  $('#address_det').css('display', 'none');
+});
+
+$('#education').toggle(function() {
+  $('#well-right').css('visibility', 'visible');
+  $('#edu_det').css('display', 'block');
+}, function() {
+  // on mouseout, reset the background colour
+  $('#well-right').css('visibility', 'hidden');
+  $('#edu_det').css('display', 'none');
+});
+
+$('#work').toggle(function() {
+  $('#well-left').css('visibility', 'visible');
+  $('#jobs_det').css('display', 'block');
+}, function() {
+  // on mouseout, reset the background colour
+  $('#well-left').css('visibility', 'hidden');
+  $('#jobs_det').css('display', 'none');
+});
+
+$('#sport').toggle(function() {
+  $('#well-left').css('visibility', 'visible');
+  $('#sport_det').css('display', 'block');
+}, function() {
+  // on mouseout, reset the background colour
+  $('#well-left').css('visibility', 'hidden');
+  $('#sport_det').css('display', 'none');
+});
+
+$('#films').toggle(function() {
+  $('#well-left').css('visibility', 'visible');
+  $('#film_det').css('display', 'block');
+}, function() {
+  // on mouseout, reset the background colour
+  $('#well-left').css('visibility', 'hidden');
+  $('#film_det').css('display', 'none');
 });
 
 $(function() {
-  $('#address').click(function() {
-    $('#well-right').css('visibility', 'visible');
-    $('#address_det').css('display', 'block');
-  }, function() {
-    // on mouseout, reset the background colour
-    $('#well-right').css('visibility', 'hidden');
-    $('#address_det').css('display', 'none');
-  });
-});
-
-$(function() {
-  $('#education').click(function() {
-    $('#well-right').css('visibility', 'visible');
-    $('#edu_det').css('display', 'block');
-  }, function() {
-    // on mouseout, reset the background colour
-    $('#well-right').css('visibility', 'hidden');
-    $('#edu_det').css('display', 'none');
-  });
-});
-
-$(function() {
-  $('#work').click(function() {
-    $('#well-left').css('visibility', 'visible');
-    $('#jobs_det').css('display', 'block');
-  }, function() {
-    // on mouseout, reset the background colour
-    $('#well-left').css('visibility', 'hidden');
-    $('#jobs_det').css('display', 'none');
-  });
-});
-
-
-$(function() {
-  $('#sport').click(function() {
-    $('#well-left').css('visibility', 'visible');
-    $('#sport_det').css('display', 'block');
-  }, function() {
-    // on mouseout, reset the background colour
-    $('#well-left').css('visibility', 'hidden');
-    $('#sport_det').css('display', 'none');
-  });
-});
-
-$(function() {
-  $('#films').click(function() {
-    $('#well-left').css('visibility', 'visible');
-    $('#film_det').css('display', 'block');
-  }, function() {
-    // on mouseout, reset the background colour
-    $('#well-left').css('visibility', 'hidden');
-    $('#film_det').css('display', 'none');
-  });
+	$('#family').click(function() {
+		$(this).toggle();
+	});
+	$('#address').click(function() {
+		$(this).toggle();
+	});
+	$('#education').click(function() {
+		$(this).toggle();
+	});
+	$('#work').click(function() {
+		$(this).toggle();
+	});
+	$('#sport').click(function() {
+		$(this).toggle();
+	});
+	$('#films').click(function() {
+		$(this).toggle();
+	});
 });
 
