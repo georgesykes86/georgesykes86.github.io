d2661db0caaead68812593e36cfc3f5c06821dc4
George Sykes <georgesykes86@gmail.com>
2018-03-11 19:41:03 +0000

    update js

--- mysitejavascript.js
+++ mysitejavascript.js
@@ -34,27 +34,21 @@ $(document).ready( function() {
 	function handler1() {
 		switch(this.id) {
 	    case "family":
-				$('#well-right').css('visibility', 'visible');
 				$('#family_det').css('display', 'block');
 	        break;
 	    case "address":
-	      $('#well-right').css('visibility', 'visible');
 	      $('#address_det').css('display', 'block');
 	        break;
 	    case "education":
-	     	$('#well-right').css('visibility', 'visible');
 	      $('#edu_det').css('display', 'block');
 	        break;
 	    case "work":
-	     	$('#well-left').css('visibility', 'visible');
 	     	$('#jobs_det').css('display', 'block');
 	        break;
 	    case "sport":
-	    	$('#well-left').css('visibility', 'visible');
 	    	$('#sport_det').css('display', 'block');
 	        break;
 	    case "films":
-	      $('#well-left').css('visibility', 'visible');
 	     	$('#film_det').css('display', 'block');
 	        break;
 	    default:
@@ -63,33 +57,7 @@ $(document).ready( function() {
 	}
 
 	function handler2() {
-		switch(this.id) {
-		  case "family":
-				$('#well-right').css('visibility', 'hidden');
-				$('#family_det').css('display', 'none');
-		      break;
-		  case "address":
-		    $('#well-right').css('visibility', 'hidden');
-		    $('#address_det').css('display', 'none');
-		      break;
-		  case "education":
-		   	$('#well-right').css('visibility', 'hidden');
-		    $('#edu_det').css('display', 'none');
-		      break;
-		  case "work":
-		   	$('#well-left').css('visibility', 'hidden');
-		   	$('#jobs_det').css('display', 'none');
-		      break;
-		  case "sport":
-		  	$('#well-left').css('visibility', 'hidden');
-		  	$('#sport_det').css('display', 'none');
-		      break;
-		  case "films":
-		    $('#well-left').css('visibility', 'hidden');
-		   	$('#film_det').css('display', 'none');
-		      break;
-		  default:
-		};
+		$(".am_container").css('display', 'none');
 		$(".small_img").one("click", handler1);
 	}
 
