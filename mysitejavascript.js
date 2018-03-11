c76989c88f1b6689e537f6a80000ed56ba227231
George Sykes <georgesykes86@gmail.com>
2018-03-11 18:14:40 +0000

    well styling

--- mysitejavascript.js
+++ mysitejavascript.js
@@ -36,10 +36,26 @@ $(function() {
     $('#well-right').css('visibility', 'hidden');
     $('#family_det').css('display', 'none');
   });
+  $('#family').onclick(function() {
+    $('#well-right').css('visibility', 'visible');
+    $('#family_det').css('display', 'block');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#well-right').css('visibility', 'hidden');
+    $('#family_det').css('display', 'none');
+  });
 });
 
 $(function() {
   $('#address').hover(function() {
+    $('#well-right').css('visibility', 'visible');
+    $('#address_det').css('display', 'block');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#well-right').css('visibility', 'hidden');
+    $('#address_det').css('display', 'none');
+  });
+  $('#address').onclick(function() {
     $('#well-right').css('visibility', 'visible');
     $('#address_det').css('display', 'block');
   }, function() {
@@ -58,10 +74,26 @@ $(function() {
     $('#well-right').css('visibility', 'hidden');
     $('#edu_det').css('display', 'none');
   });
+  $('#education').onclick(function() {
+    $('#well-right').css('visibility', 'visible');
+    $('#edu_det').css('display', 'block');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#well-right').css('visibility', 'hidden');
+    $('#edu_det').css('display', 'none');
+  });
 });
 
 $(function() {
   $('#work').hover(function() {
+    $('#well-left').css('visibility', 'visible');
+    $('#jobs_det').css('display', 'block');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#well-left').css('visibility', 'hidden');
+    $('#jobs_det').css('display', 'none');
+  });
+  $('#work').onclick(function() {
     $('#well-left').css('visibility', 'visible');
     $('#jobs_det').css('display', 'block');
   }, function() {
@@ -81,10 +113,26 @@ $(function() {
     $('#well-left').css('visibility', 'hidden');
     $('#sport_det').css('display', 'none');
   });
+  $('#sport').onclick(function() {
+    $('#well-left').css('visibility', 'visible');
+    $('#sport_det').css('display', 'block');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#well-left').css('visibility', 'hidden');
+    $('#sport_det').css('display', 'none');
+  });
 });
 
 $(function() {
   $('#films').hover(function() {
+    $('#well-left').css('visibility', 'visible');
+    $('#film_det').css('display', 'block');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#well-left').css('visibility', 'hidden');
+    $('#film_det').css('display', 'none');
+  });
+  $('#films').onclick(function() {
     $('#well-left').css('visibility', 'visible');
     $('#film_det').css('display', 'block');
   }, function() {
